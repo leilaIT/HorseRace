fca030514f64c85fdfeaee21731b89bd8815f67d
leilaIT <delarealeila@gmail.com>
2023-11-08 01:32:28 +0800

    Add project files.

---
+++ HorseRace/Horse.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    internal class Horse
+    {
+        private int hNum = 0;
+        private int hPos = 0;
+        private ConsoleColor hColor;
+        static Random rnd = new Random();
+        private string hName = "";
+        public Horse(int num, ConsoleColor color) 
+        { 
+            hNum = num;
+            hColor = color;
+            hName = hNum.ToString();
+            while(hName.Length != 3)
+                hName = "0" + hName;
+            displayHorses();
+        }
+        public void displayHorses()
+        {
+            Console.ForegroundColor = hColor;
+            Console.Write($"{hName}: ");
+            Console.Write(new string(' ', hPos) + "X");
+            Console.WriteLine(new string(' ', 40)/* + "|"*/);
+            Console.ResetColor();
+        }
+        public void move()
+        {
+            int hMove = 0;
+            int hStep = 0;
+            
+            hMove = rnd.Next(1, 4);
+
+            hStep = rnd.Next(0, 2);
+
+            if(hStep == 0)
+                hPos -= hMove;
+            else
+                hPos += hMove;
+
+            if (hPos < 0)
+                hPos = 0;
+            else if (hStep > 40)
+                hPos = 40;
+            
+            displayHorses();
+            //starts the game
+            //this will call methods that will:
+            //  get random number for movement so X will ++ or --
+            //  display updated horse movements
+
+        }
+    }
+}
---
+++ HorseRace/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HorseRace
+{
+    //possible flow??
+    //initialize all horses in list [DONE BTCH]
+    //display all horses in list [DONE]
+    //get random movement
+    //check victory status for each horse in each movement
+    //  if not at finish line yet,
+    //      keep moving
+    //  else (at finish line)
+    //      break (stop the game and declare winner!!
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //initialize horses
+            List<Horse> horses = new List<Horse>();
+            horses.Add(new Horse(1, ConsoleColor.Cyan));
+            horses.Add(new Horse(2, ConsoleColor.DarkYellow));
+            horses.Add(new Horse(3, ConsoleColor.Blue));
+            horses.Add(new Horse(4, ConsoleColor.Yellow));
+            Console.WriteLine("Press any key to start race. . .");
+            Console.ReadKey();
+            Console.Clear();
+            bool cont = true;
+
+            while(cont)
+            {
+                foreach (Horse horse in horses)
+                {
+                    horse.move();
+                }
+                if (!cont)
+                    break;
+                Console.ReadKey();
+                Console.Clear();
+
+            }
+            //Horse horse = new Horse();
+
+            Console.ReadKey();
+        }
+    }
+}
